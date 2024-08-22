1bdb750f316073da89a7a81db8fe72e1e31f74f0
Ony <onyrakoto27@gmail.com>
2024-08-22 22:46:50 +0300

    update nixosMOdules

--- nix/nixosModule.nix
+++ nix/nixosModule.nix
@@ -2,35 +2,37 @@ presentationPackage: {
   lib,
   config,
   ...
-}:
-with lib; {
-  options.presentation = {
-    enable = mkEnableOption "My awesome nix presentation";
+}: let
+  cfg = config.programs.nix-presentation;
+in
+  with lib; {
+    options.programs.nix-presentation = {
+      enable = mkEnableOption "My awesome nix presentation";
 
-    port = mkOption {
-      type = types.port;
-      default = 8080;
-      description = "The port on which the presentation will be served.";
-    };
-  };
-
-  config = mkIf config.presentation.enable {
-    services.nginx = {
-      enable = true;
-      virtualHosts.localhost = {
-        listen = [
-          {
-            addr = "0.0.0.0";
-            port = config.presentation.port;
-          }
-        ];
-        locations."/" = {
-          root = "${presentationPackage}";
-          index = "index.html";
-        };
+      port = mkOption {
+        type = types.port;
+        default = 8080;
+        description = "The port on which the presentation will be served.";
       };
     };
 
-    networking.firewall.allowedTCPPorts = [config.presentation.port];
-  };
-}
+    config = mkIf cfg.enable {
+      services.nginx = {
+        enable = true;
+        virtualHosts.localhost = {
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = config.presentation.port;
+            }
+          ];
+          locations."/nix-presentation" = {
+            root = "${presentationPackage}";
+            index = "index.html";
+          };
+        };
+      };
+
+      networking.firewall.allowedTCPPorts = [config.presentation.port];
+    };
+  }
